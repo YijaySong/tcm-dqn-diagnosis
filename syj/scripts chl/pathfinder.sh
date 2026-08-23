409f760d6ce73c03a86dada8dfd803800263db0d
YijaySong <143768883+YijaySong@users.noreply.github.com>
2026-08-23 21:59:44 +0800

    Delete syj/scripts chl directory

--- syj/scripts chl/pathfinder.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#relation=$base@schemastaging@organization_extra@phone_number.@base@schemastaging@phone_sandbox@service_location
-relation="concept_agentbelongstoorganization"
-python sl_policy.py $relation
-python policy_agent.py $relation retrain
-python policy_agent.py $relation test
-
